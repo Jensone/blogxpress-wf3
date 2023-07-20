841ef39fe22b61afc053d9815aef84f527057c2d
Jensone <hello@jensone.com>
2023-07-20 22:02:15 +0200

    🖼️ add(public): add bx-line-dark.png image to public directory 🎨 style(style.css): add new CSS class .bx-meteor and apply background image and text shadow styles 🖼️ add(public): add meteor.svg file to public directory 🔧 chore(ArticleType.php): update ArticleType form to include labels, attributes, and entity types for title, content, category, author fields, and add submit button
    🔧 fix(base.html.twig): refactor navigation bar to improve readability and responsiveness
    🔧 fix(home.html.twig): add rounded and py-5 classes to the main row for better styling

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -2,25 +2,55 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label' => 'Titre de l\'article',
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
             ->add('content', CKEditorType::class, array(
                 'config' => array(
                     'uiColor' => '#ffffff',
-                ),
-            ))
-            ->add('category')
-            ->add('author')
+                    'toolbar' => 'basic',
+                ))
+            )
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'attr' => [
+                    'class' => 'form-control mt-3 mb-3'
+                ],
+            ])
+            ->add('author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name',
+                'label' => 'Auteur',
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ],
+            ])
+            ->add('Publier', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary rounded-pill mb-3'
+                ]
+            ])
         ;
     }
 
