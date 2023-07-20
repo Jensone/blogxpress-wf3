07dd38d6f6ce10aed51850d1e5f976293a99ac82
Jensone <hello@jensone.com>
2023-07-20 11:14:58 +0200

    🔧 fix(HomeController.php): add Request parameter to contact() method to handle form submission and data retrieval ✨ feat(HomeController.php): handle form submission and retrieve form data in contact() method 🔧 fix(contact.html.twig): remove dump() function call for debugging purposes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\ContactType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,12 +20,29 @@ class HomeController extends AbstractController
     }
 
     #[Route('/contact', name: 'contact', methods: ['GET', 'POST'])]
-    public function contact(): Response
+    public function contact(Request $request): Response
     {
         $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On stocke les données du formulaire dans des variables
+            $nom =  $form->get('Nom')->getData();
+            $prenom =  $form->get('Prenom')->getData();
+            $sujet =  $form->get('Sujet')->getData();
+            $email =  $form->get('Email')->getData();
+            $tel = $form->get('Telephone')->getData();
+            $message =  $form->get('Message')->getData();
+
+            // On envoie l'email avec les données du formulaire
+
+        }
+
+        $result = $form->getData();
         
         return $this->render('home/contact.html.twig', [
             'contactForm' => $form,
+            'result' => $result,
         ]);
     }
 }
